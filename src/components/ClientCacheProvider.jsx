b0130b59647f85b327f81f7ebba07ec08e9de4b1
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2026-07-28 14:14:58 +0700

    fix rtl and ltr style

--- src/components/ClientCacheProvider.jsx
+++ src/components/ClientCacheProvider.jsx
@@ -1,9 +1,22 @@
 'use client'
 
 import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+import rtlPlugin from 'stylis-plugin-rtl'
+import { prefixer } from 'stylis'
+import { useMemo } from 'react'
+import { useSettings } from '@core/hooks/useSettings'
 
-const ClientCacheProvider = ({ value, children }) => {
-  return <CacheProvider value={value}>{children}</CacheProvider>
+const ClientCacheProvider = ({ children }) => {
+  const { settings } = useSettings()
+  const direction = settings?.direction || 'ltr'
+
+  const cache = useMemo(() => {
+    const stylisPlugins = direction === 'rtl' ? [prefixer, rtlPlugin] : []
+    return createCache({ key: direction === 'rtl' ? 'mui-rtl' : 'mui', prepend: true, stylisPlugins })
+  }, [direction])
+
+  return <CacheProvider value={cache}>{children}</CacheProvider>
 }
 
 export default ClientCacheProvider
--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -3,10 +3,7 @@ import { HorizontalNavProvider } from '@menu/contexts/horizontalNavContext'
 import { SettingsProvider } from '@core/contexts/settingsContext'
 import CustomThemeProvider from './theme'
 import { getMode, getSettingsFromCookie } from '@core/utils/serverHelpers'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import ClientCacheProvider from './ClientCacheProvider'
-import rtlPlugin from 'stylis-plugin-rtl'
-import { prefixer } from 'stylis'
 
 const Providers = async props => {
   const { children } = props
@@ -14,26 +11,19 @@ const Providers = async props => {
   const mode = await getMode()
   const settingsCookie = await getSettingsFromCookie()
 
-  const direction = settingsCookie?.direction || 'ltr'
-  const cacheOptions = {
-    prepend: true,
-    key: direction === 'rtl' ? 'mui-rtl' : 'mui',
-    stylisPlugins: direction === 'rtl' ? [prefixer, rtlPlugin] : []
+    return (
+      <SettingsProvider settingsCookie={settingsCookie} mode={mode}>
+        <VerticalNavProvider>
+          <HorizontalNavProvider>
+            <ClientCacheProvider>
+              <CustomThemeProvider>
+                {children}
+              </CustomThemeProvider>
+            </ClientCacheProvider>
+          </HorizontalNavProvider>
+        </VerticalNavProvider>
+      </SettingsProvider>
+    )
   }
 
-  return (
-    <SettingsProvider settingsCookie={settingsCookie} mode={mode}>
-      <VerticalNavProvider>
-        <HorizontalNavProvider>
-          <AppRouterCacheProvider CacheProvider={ClientCacheProvider} options={cacheOptions}>
-            <CustomThemeProvider>
-              {children}
-            </CustomThemeProvider>
-          </AppRouterCacheProvider>
-        </HorizontalNavProvider>
-      </VerticalNavProvider>
-    </SettingsProvider>
-  )
-}
-
-export default Providers
+  export default Providers
