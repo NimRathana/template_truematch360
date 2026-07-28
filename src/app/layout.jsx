65bcb81a1978e01cbf205db2c3891affd8d5ba1a
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2026-07-28 16:22:28 +0700

    commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,19 @@
 import 'react-perfect-scrollbar/dist/css/styles.css'
 import './globals.css'
 import 'remixicon/fonts/remixicon.css';
-import Providers from '@/components/Providers'
-import LocalizationProviderWrapper from '@/components/LocalizationProviderWrapper'
-import AuthLayoutSwitcher from '@components/AuthLayoutSwitcher'
 
 export const metadata = {
   title: 'Rathana Template',
   description: 'Develop next-level web apps with Rathana Template - NextJS Admin Dashboard Template. Now, updated with lightning-fast routing powered by MUI and App router.'
 }
 
-const RootLayout = async ({ children }) => {
+const RootLayout = ({ children }) => {
   const direction = 'ltr'
 
   return (
     <html id='__next' dir={direction}>
       <body className='flex is-full min-bs-full flex-auto flex-col'>
-        <LocalizationProviderWrapper>
-          <Providers>
-            <AuthLayoutSwitcher>{children}</AuthLayoutSwitcher>
-          </Providers>
-        </LocalizationProviderWrapper>
+        {children}
       </body>
     </html>
   )
